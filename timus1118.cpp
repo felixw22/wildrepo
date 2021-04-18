866a0da5039323629275bee17dd69c5fa07fa947
felixw22 <81265103+felixw22@users.noreply.github.com>
2021-04-18 11:28:07 +0200

    Update timus1118.cpp

--- timus1118.cpp
+++ timus1118.cpp
@@ -17,8 +17,9 @@ int main()
 	cin >> i >> j;
 	pair<int, float> minim;
 	minim.second = 10000000;
-	for (int k = i; k<=j; k++){
+	for (int k = j; k>=i; k--){
 		float curerg = getVal(k);
+		if (getVal(k) == 1.0 / k) return k;
 		if (curerg < minim.second) {
 			minim.first = k;
 			minim.second = curerg;
